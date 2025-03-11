df78e251dc835fe641f19e2e4f9d7a58e22e0c92
Rajvinayallam <allamrajvinay@gmail.com>
2025-03-11 12:01:43 +0530

    Test

--- src/com/Main.java
+++ src/com/Main.java
@@ -1,12 +1,14 @@
 package com;
 
+import com.model.Author;
 import com.model.Book;
 
 public class Main {
 	public static void main(String[] args) {
 		Book book = new Book();
-		book.setTitle("Complete Java");
-		book.setAuthor("Herbert Schildt");
+		book.setTitle("Harry Porter");
+		Author author = new Author();
+		author.setAuthor("Harry");
 		System.out.println(book);
 	}
 }
---
+++ src/com/model/Author.java
@@ -0,0 +1,14 @@
+package com.model;
+
+public class Author {
+	private String Author;
+
+	public String getAuthor() {
+		return Author;
+	}
+	public void setAuthor(String author) {
+		Author = author;
+	}
+	
+	
+}
